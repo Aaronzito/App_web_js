3d438a867de7c515398e86a0b66552efd7d17499
Aaronzito <aaroncuen020714@gmail.com>
2023-01-16 08:25:17 -0700

    Se consumió el API

---
+++ forEach.js
@@ -0,0 +1,27 @@
+/* const edades = [22, 4, 5, 8, 9];
+edades.forEach((valor) =>{
+console.log(valor);
+}); */
+
+const usuarios=[
+    {
+        nombre:'Aaroncito',
+        edad:20,
+        genero: 'M'
+
+    },
+    {
+        nombre: 'Etolelualo',
+        edad: 14,
+        Genero: 'F'
+    },
+    {
+        nombre:'Añañ',
+        edad: '20',
+        genero: 'F'
+    }   
+
+
+];
+usuarios.forEach((usuario)=>{console.log(usuario.nombre);
+});
